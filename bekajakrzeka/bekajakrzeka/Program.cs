324fb95e30e1ac1bfad2b4998aeb66ad332e8de8
Simon0824 <ez2137aaa@gmail.com>
2024-10-24 09:33:29 +0200

    xd

---
+++ bekajakrzeka/bekajakrzeka/Program.cs
@@ -0,0 +1,40 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        wieksza();
+    }
+
+    static void wieksza()
+    {
+        int a;
+        int b;
+
+        Podawanie:
+
+        Console.WriteLine("Podaj pierwszą liczbę");
+        bool liczba1 = int.TryParse(Console.ReadLine(),out a);
+
+        Console.WriteLine("Podaj drugą liczbę");
+        bool liczba2 = int.TryParse(Console.ReadLine(), out b);
+
+        while (liczba1 == false || liczba2 == false)
+        {
+            Console.WriteLine("Nie podałeś liczby");
+            goto Podawanie;
+        }
+
+        if (a > b)
+        {
+            Console.WriteLine("Pierwsza liczba jest większa");
+        }
+        else if (b > a)
+        {
+            Console.WriteLine("Druga liczba jest większa");
+        }
+        else if(a == b)
+        {
+            Console.WriteLine("Liczby są równe");
+        }
+    }
+}
